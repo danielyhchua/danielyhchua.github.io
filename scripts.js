793d81e4f7bd1a4ce60167ba8ff5bac71314eb58
Daniel Chua <danielchua@thinkpursuits.com>
2020-10-24 22:37:44 +1100

    Update resume page

--- scripts.js
+++ scripts.js
@@ -3,36 +3,21 @@
  *   NAVBAR
  *
  *******************/
-// When the user scrolls the page, execute functions
-window.onscroll = function() {
-  addSticky();
-  addActive();
-};
-
 // Get the navbar
 var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
 // Get position of all sections
 var workExpPos = document.querySelector("#work-experience").offsetTop;
 var eduPos = document.querySelector("#education").offsetTop;
 var skillsPos = document.querySelector("#skills").offsetTop - 1;
-// var footer = document.querySelector("footer");
-// var footerPos = footer.offsetTop;
 
 // Get nav li items
-var navList = document.querySelectorAll(".nav-item");
+var navList = document.querySelectorAll(".float-nav-item");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function addSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky-nav")
-  } else {
-    navbar.classList.remove("sticky-nav");
-  }
-}
+// When the user scrolls the page, execute functions
+window.onscroll = function() {
+  addActive();
+};
 
 // Add active class for correct scroll position
 function addActive() {
@@ -43,8 +28,15 @@ function addActive() {
     posIndex = 1;
   } else if (window.pageYOffset >= eduPos && window.pageYOffset < skillsPos) {
     posIndex = 2;
+  } else if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+    posIndex = 3;
   } else if (window.pageYOffset >= skillsPos) {
     posIndex = 3;
+  }
+  if (posIndex > 0) {
+    console.log("remove");
+    console.log(navList[0]);
+    navList[0].classList.remove("active");
   }
   navList.forEach((item, i) => {
     if (posIndex === i) {
